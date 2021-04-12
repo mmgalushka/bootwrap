2df53836b2979d6a36b8ae4404ab373f8758ca4b
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-04-11 20:50:20 -0400

    feat: add shorthand methods to set margin classes

--- bootwrap/components/base.py
+++ bootwrap/components/base.py
@@ -107,6 +107,124 @@ class ClassMixin:
             return ' '.join(self.__classes)
         return None
 
+    def m(self, size):
+        """Sets margin for all four sides to the specified size.
+
+        Args:
+            size (int): Size of the margin to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").m(3)
+        """
+        return self.add_classes(f"m-{size}")
+
+    def mt(self, size):
+        """Sets margin for the top side to the specified size.
+
+        Args:
+            size (int): Size of the margin to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").mt(3)
+        """
+        return self.add_classes(f"mt-{size}")
+
+    def mb(self, size):
+        """Sets margin for the bottom side the specified size.
+
+        Args:
+            size (int): Size of the margin to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").mb(3)
+        """
+        return self.add_classes(f"mb-{size}")
+
+    def ml(self, size):
+        """Sets margin for the left side to the specified size.
+
+        Args:
+            size (int): Size of the margin to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").ml(3)
+        """
+        return self.add_classes(f"ml-{size}")
+
+    def mr(self, size):
+        """Sets margin for the right side to the specified size.
+
+        Args:
+            size (int): Size of the margin to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").mr(3)
+        """
+        return self.add_classes(f"mr-{size}")
+
+    def mx(self, size):
+        """Sets margin for left and right sides to the specified size.
+
+        Args:
+            size (int): Size of the margin to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").mx(3)
+        """
+        return self.add_classes(f"mx-{size}")
+
+    def my(self, size):
+        """Sets margin for top and bottom sides to the specified size.
+
+        Args:
+            size (int): Size of the margin to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").my(3)
+        """
+        return self.add_classes(f"my-{size}")
 
 class ActionMixin:
     """Mixin for a web component which able to perform an action."""
