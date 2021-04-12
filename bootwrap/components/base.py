f00c79b386d5fddb4c7abf8d34b726a291bea4a4
Andrés Ignacio Torres <andresitorresm@gmail.com>
2021-04-11 20:53:16 -0400

    feat: add shorthand methods to set padding classes

--- bootwrap/components/base.py
+++ bootwrap/components/base.py
@@ -226,6 +226,125 @@ class ClassMixin:
         """
         return self.add_classes(f"my-{size}")
 
+    def p(self, size):
+        """Sets padding for all four sides to the specified size.
+
+        Args:
+            size (int): Size of the padding to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").p(3)
+        """
+        return self.add_classes(f"p-{size}")
+
+    def pt(self, size):
+        """Sets padding for the top side to the specified size.
+
+        Args:
+            size (int): Size of the padding to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").pt(3)
+        """
+        return self.add_classes(f"pt-{size}")
+
+    def pb(self, size):
+        """Sets padding for the bottom side the specified size.
+
+        Args:
+            size (int): Size of the padding to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").pb(3)
+        """
+        return self.add_classes(f"pb-{size}")
+
+    def pl(self, size):
+        """Sets padding for the left side to the specified size.
+
+        Args:
+            size (int): Size of the padding to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").pl(3)
+        """
+        return self.add_classes(f"pl-{size}")
+
+    def pr(self, size):
+        """Sets padding for the right side to the specified size.
+
+        Args:
+            size (int): Size of the padding to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").pr(3)
+        """
+        return self.add_classes(f"pr-{size}")
+
+    def px(self, size):
+        """Sets padding for left and right sides to the specified size.
+
+        Args:
+            size (int): Size of the padding to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").px(3)
+        """
+        return self.add_classes(f"px-{size}")
+
+    def py(self, size):
+        """Sets padding for top and bottom sides to the specified size.
+
+        Args:
+            size (int): Size of the padding to set.
+
+        Returns:
+            obj (self): The instance of this class.
+
+        Example:
+            from bootwrap import Button
+
+            # Note, that Button inherits ClassMixin.
+            button = Button("Hello").py(3)
+        """
+        return self.add_classes(f"py-{size}")
+
 class ActionMixin:
     """Mixin for a web component which able to perform an action."""
     def __init__(self):
