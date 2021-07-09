808e92dccdc20151d89c64c96ee35d26f5c6359f
Mykola Galushka <mm.galushka@gmail.com>
2021-07-09 13:59:46 +0100

    Fixed #20: corrected flake8 annotation.

--- bootwrap/__init__.py
+++ bootwrap/__init__.py
@@ -4,9 +4,9 @@ Wrapper for Bootstrap components.
 
 __version__ = '1.0.0-alpha4'
 
-from .menu import Menu                 # NOQA
-from .page import Page                 # NOQA
+from .menu import Menu                 # NOQA: F401
+from .page import Page                 # NOQA: F401
 
 # The components __init__.py includes an exact list of importing elements,
 # so we are safe to use the '*' import hear.
-from .components import *              # NOQA
+from .components import *              # NOQA: F401, F403
